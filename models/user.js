42c8a324224763ed4ff4ed7e2ebea053f5e8b041
Nikolai Hesterberg <nikolai.hesterberg@gmail.com>
2024-05-04 16:08:46 +0200

    user model developed

---
+++ models/user.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose')
+
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true
+  },
+  name: {
+    type: String,
+    required: false
+  },
+  passwordHash: {
+    type: String,
+    required: true
+  }
+})
+
+userSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+    // passwordHash shouldn't be returned
+    delete returnedObject.passwordHash
+  }
+})
+
+const User = mongoose.model('User', userSchema)
+
+module.exports = User
